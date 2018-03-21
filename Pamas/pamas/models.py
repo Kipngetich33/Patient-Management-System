9d69f42ebc9298c2e1c2e88a3f14aa5cc48e3e8a
Kipngetich33 <khalifngeno@gmail.com>
2018-03-21 12:59:15 -0400

    update the models and migrate

--- Pamas/pamas/models.py
+++ Pamas/pamas/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
+class Profile(models.Model):
+    user_type = model.IntegerField
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     email = models.EmailField()
+    hospital = models.CharField(max_length =30)
+    location = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.first_name
+
+class Appointment(models.Model):
+    type_of_appointment = models.CharField(max_length =30)
+    appointement_time = models.CharField(max_length =30)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    doctor = models.ForeignKey(User,on_delete=models.CASCADE)
+    patient = models.ForeignKey(User,on_delete=models.CASCADE)
+    status = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.type_of_appointment
+
+
